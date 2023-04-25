5dc00e0d3839f7ebe645abc843c33ef297dd42ab
jinook929 <jinook929@gmail.com>
2023-04-25 18:06:00 -0400

    add pixel hovering page card to animations

--- src/app/animations/page.tsx
+++ src/app/animations/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import IconTrailing from "../../../public/assets/images/icon-trailing.png";
+import PixelHovering from "../../../public/assets/images/pixel-hovering.png";
 import Link from "next/link";
 
 export default function page() {
@@ -12,13 +13,22 @@ export default function page() {
       </section>
       <div className={styles.cards}>
         <div className={styles.card}>
-          <Link href="/animations/icon-trailing">
-            <Image src={IconTrailing} alt="icon trailing capture" />
+          <Link className={styles.cardImage} href="/animations/icon-trailing">
+              <Image src={IconTrailing} alt="icon trailing capture" fill />
           </Link>
           <Link href="/animations/icon-trailing">
             <h2>Icon Trailing</h2>
           </Link>
           <p>Your mouse movement will trigger icons to light!</p>
+        </div>
+        <div className={styles.card}>
+          <Link className={styles.cardImage} href="/animations/pixel-hovering">
+              <Image src={PixelHovering} alt="icon trailing capture" fill />
+          </Link>
+          <Link href="/animations/pixel-hovering">
+            <h2>Pixel Hovering</h2>
+          </Link>
+          <p>Pixels will appear as your mouse hovers over!</p>
         </div>
       </div>
     </main>
